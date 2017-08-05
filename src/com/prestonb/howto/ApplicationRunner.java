c95bb29219846bee3019f228421f22d2e54d789b
Preston Briggs <pbriggs28@users.noreply.github.com>
2017-08-04 18:03:44 -0700

    Read from console

--- src/com/prestonb/howto/ApplicationRunner.java
+++ src/com/prestonb/howto/ApplicationRunner.java
@@ -1,9 +1,75 @@
 package com.prestonb.howto;
 
-public class ApplicationRunner {
-	
-	public void run() {
-		
-	}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
+public class ApplicationRunner {
+
+	public void run() {
+		readFromConsole();
+	}
+	
+	private void readFromConsole() {
+		boolean exit = false;
+		
+		// Step 1: Get the console as an "input stream"
+		Reader consoleInput = new InputStreamReader(System.in);
+		BufferedReader br = null;
+		
+		try {
+
+			// Step 2: Create a BufferedReader from the InputStreamReader
+			// This allows reading full lines at a time
+			br = new BufferedReader(consoleInput);
+
+			while (exit == false) {
+				
+				// Step 3: Print to the console to tell the user what to do
+				System.out.print("Enter 'exit' to quit application: ");
+
+				// Step 4: Tell the BufferedReader to tell the InputStreamReader to 
+				// wait for the user to enter the next line
+				
+				// BufferedReader -> InputStreamReader -> console -> [user types something]
+				//																|
+				//																|
+				//																|
+				// BufferedReader <- InputStreamReader <- console <-------------- 
+				String input = br.readLine();
+
+				// Step 5: Check if the user typed "exit"
+				if (input == "exit") {
+//				if ("exit".equals(input)) {
+					exit = true;
+					System.out.println("Fine I'll just leave then..");
+				} else {
+					
+					// Step 6: Print what the user typed
+					System.out.println("You typed : " + input);
+					System.out.println("-----------\n");
+				}
+			}
+			
+		// If something unexpected happens an exception will be thrown and caught here
+		} catch (IOException e) {
+			System.out.println("An error occured!");
+			e.printStackTrace();
+		} finally {
+			
+			// Step 7: VERY IMPORTANT! Close the input stream. If not it will stay open forever.
+			// For a console InputStream this isn't a big deal but if reading from a file
+			// it could keep a lock on the file and no one could edit it
+			if (br != null) {
+				try {
+					System.out.println("Closing input stream");
+					br.close();
+				} catch (IOException e) {
+					System.out.println("An error occured!");
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
